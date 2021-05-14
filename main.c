f08cd5d8d1d1b9539d651160c4396fd5b1ab3afd
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-05-13 22:17:26 -0300

    updated error mssg in main

--- main.c
+++ main.c
@@ -15,14 +15,14 @@ int main(int argc, char *argv[])
 	/* arg error */
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(1);
 	}
 	/* open file error */
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
-		printf("Error: Can't open file %s", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s", argv[1]);
 		exit(1);
 	}
 	read_file(fp);
