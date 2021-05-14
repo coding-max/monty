a31195f7e40b31d24272c9ae6fcfe5af3d13bbce
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-05-13 22:22:46 -0300

    updated error mssg in main

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	read_file(fp);
